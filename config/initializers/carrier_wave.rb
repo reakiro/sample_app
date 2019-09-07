2cd75f01e54b533e1c1aa5186e2adc34efb4fd83
reakiro <platypusdarina@gmail.com>
2019-09-07 14:06:30 +0300

    Added for provider

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/google'
     config.fog_credentials = {
       :provider              => 'Google',
       :google_storage_access_key_id     => ENV['GOOG1EHA7WUD2LUF6I2SLEAUELZHA6JZNR7WZW7PLYTWZL3JYADNXB4WWL5VA'],
