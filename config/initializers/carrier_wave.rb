096d453f6e2cb200b0691827500cd42df1aabf81
reakiro <platypusdarina@gmail.com>
2019-09-07 13:47:31 +0300

    Reducted keys info

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,9 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Configuration for Amazon S3
       :provider              => 'Google',
-      :aws_access_key_id     => ENV['GOOG1EHA7WUD2LUF6I2SLEAUELZHA6JZNR7WZW7PLYTWZL3JYADNXB4WWL5VA'],
-      :aws_secret_access_key => ENV['lofRUal5xzALpFwk8ykXBgOsDJXCvjKEqllfMwC0']
+      :google_storage_access_key_id     => ENV['GOOG1EHA7WUD2LUF6I2SLEAUELZHA6JZNR7WZW7PLYTWZL3JYADNXB4WWL5VA'],
+      :google_storage_secret_access_key => ENV['lofRUal5xzALpFwk8ykXBgOsDJXCvjKEqllfMwC0']
     }
     config.fog_directory     =  ENV['reakiro']
   end
