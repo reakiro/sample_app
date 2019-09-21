21c4da8a66036fb270f971a1a51afa0157e63b09
reakiro <platypusdarina@gmail.com>
2019-09-21 13:45:43 +0300

    reducted fog credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/google'
     config.fog_credentials = {
-      :provider              => 'Google',
-      :google_storage_access_key_id     => ENV['GOOG1EHA7WUD2LUF6I2SLEAUELZHA6JZNR7WZW7PLYTWZL3JYADNXB4WWL5VA'],
-      :google_storage_secret_access_key => ENV['lofRUal5xzALpFwk8ykXBgOsDJXCvjKEqllfMwC0']
+      :provider                         => 'Google',
+      :google_storage_access_key_id     => 'GOOG1EHA7WUD2LUF6I2SLEAUELZHA6JZNR7WZW7PLYTWZL3JYADNXB4WWL5VA',
+      :google_storage_secret_access_key => 'lofRUal5xzALpFwk8ykXBgOsDJXCvjKEqllfMwC0'
     }
-    config.fog_directory     =  ENV['reakiro']
+    config.fog_directory = 'reakiro'
   end
 end
